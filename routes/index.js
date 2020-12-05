c8b6dfbae7f653c2e468563ae1fa556d9bc1187f
Deepak Jain <dj524575@gmail.com>
2020-12-05 15:44:18 +0530

    Setup view engine

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const { route } = require('./users');
+
+
 console.log('router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+// router.use('/users',require('./posts'));
+router.use('/posts',require('./posts'));
 
+//For any further routes,access from here
+//router.use('/routerName',require('./routerName'));
 module.exports = router;
