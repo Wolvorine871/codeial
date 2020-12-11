abdc51074d8f4f74261ae0d8350b84610e7d992a
Deepak Jain <dj524575@gmail.com>
2020-12-11 16:37:08 +0530

    Passport Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,6 +16,9 @@ module.exports.posts = function(req,res){
 
 //render the signup page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title: "Codeial | Sign Up"
     });
@@ -23,6 +26,9 @@ module.exports.signUp = function(req,res){
 
 //render the signin page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title: "Codeial | Sign In"
     });
@@ -35,7 +41,7 @@ module.exports.create = function(req,res){
         return res.redirect('back');
     }
     User.findOne({email: req.body.email},function(err,user){
-        if(err){console.log('errpr in finding user in signing up'); return}
+        if(err){console.log('error in finding user in signing up'); return}
 
         if(!user){
             User.create(req.body,function(err,user){
@@ -49,6 +55,11 @@ module.exports.create = function(req,res){
     });
 }
 //  //get the sign in and create a session for user 
-// module.exports.create-session = function(req,res){
-//     //TODO later
-// } 
+module.exports.createSession = function(req,res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+    return res.redirect('/');
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile',usersController.profile);
+router.get('/profile',passport.checkAuthentication,usersController.profile);
 
 router.get('/sign-in',usersController.signIn);
 
@@ -13,5 +15,13 @@ router.post('/create',usersController.create);
 
 router.get('/posts',usersController.posts);
 
+//use passsport as a middleware to authenticate
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'}
+    ) ,usersController.createSession);
+
+
+router.get('/sign-out',usersController.destroySession);
 
 module.exports = router;
